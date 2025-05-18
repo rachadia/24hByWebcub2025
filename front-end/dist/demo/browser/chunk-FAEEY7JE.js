fe6a0558ba43a5f410833e2e7aef304f699d0300
Adalio RACHADI <adalio_rachadi@carrefour.com>
2025-05-18 05:32:30 +0300

    git build with theme

---
+++ front-end/dist/demo/browser/chunk-FAEEY7JE.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-SPDKP6Y3.js";import"./chunk-OX6UQBEQ.js";import"./chunk-ZHZY3XIT.js";export{a as ThemeDemoComponent};
---
+++ front-end/dist/demo/browser/chunk-WPHHPEL3.js
@@ -0,0 +1,1 @@
+var o={production:!1,apiUrl:"https://api.lesmakiscodeu.mayotte.webcup.hodi.host/api"};export{o as a};
