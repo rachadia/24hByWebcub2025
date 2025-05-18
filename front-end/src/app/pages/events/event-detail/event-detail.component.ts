cd51287f0ba4c82521f2fce2ae497eff39bf1e5e
Adalio RACHADI <adalio_rachadi@carrefour.com>
2025-05-18 06:33:40 +0300

    theme apply

--- front-end/src/app/pages/events/event-detail/event-detail.component.ts
+++ front-end/src/app/pages/events/event-detail/event-detail.component.ts
@@ -8,6 +8,8 @@ import { Event, Comment } from '../../../models/event.model';
 import { User } from '../../../models/user.model';
 import { EmotionService } from '../../../services/emotion.service';
 import { ThemeService } from '../../../services/theme.service';
+import { THEMES } from '../../../utils/theme-utils';
+import { appliquerTheme } from '../../../utils/theme-utils';
 
 @Component({
   selector: 'app-event-detail',
@@ -216,6 +218,7 @@ export class EventDetailComponent implements OnInit {
     const eventId = this.route.snapshot.paramMap.get('id');
     if (eventId) {
       this.loadEvent(eventId);
+      this.switchTheme(this.event?.emotion || '');
     }
   }
 
@@ -340,4 +343,23 @@ export class EventDetailComponent implements OnInit {
       console.error('Failed to copy text: ', err);
     });
   }
+
+  switchTheme(theme: string): void {
+    
+    switch (theme) {
+      case 'joy':
+        appliquerTheme(THEMES.INTENSITY);
+        break;
+      case 'sadness':
+        //this.themeSubject.next('sadness');
+        appliquerTheme(THEMES.LIGHT_MELANCHOLY);
+        break;
+      case 'anger':
+        //this.themeSubject.next('anger');
+        appliquerTheme(THEMES.DARK_PASSION);
+        break;
+    default:
+      break;
+  }
+}
 }
